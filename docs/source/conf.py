60071ac75bd8d6ec9e3f9cafef3f0e54fd728b8d
FriendlyGuardian <tobin.rumsey@gmail.com>
2025-11-25 17:54:20 -0700

    initial changes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Broombots Learning Resources'
+copyright = '2025 Tobin Rumsey'
+author = 'Tobin Rumsey'
 
 release = '0.1'
 version = '0.1.0'
